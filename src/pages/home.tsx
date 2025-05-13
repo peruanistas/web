2994beb31125cd826e28969bd7eb05acdd5ac64f
Paolo Flores <pflores.fisi22@gmail.com>
2025-05-12 20:48:43 -0500

    add home base styles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,12 @@
-import { HomeFeed } from "@/components/feed";
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
-import { HomeBanner } from "@/components/home_banner";
+import { HomeFeed } from '@/components/feed';
+import { Footer } from '@/components/footer';
+import { Header } from '@/components/header';
+import { HomeBanner } from '@/components/home_banner';
+import '@styles/home.scss';
 
 export function HomePage() {
   return (
-    <main>
+    <main className='home-wrapper'>
       <Header />
       <HomeBanner />
       <HomeFeed />
