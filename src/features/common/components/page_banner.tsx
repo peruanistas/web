85a897a993eb6401382cf134598b47b51f46a98d
Paolo Flores <pflores.fisi22@gmail.com>
2025-05-17 11:06:40 -0500

    add page banner image

--- src/features/common/components/page_banner.tsx
+++ src/features/common/components/page_banner.tsx
@@ -1,4 +1,4 @@
-import bannersmall from '@assets/images/banner_sm.png';
+import bannersmall from '@assets/images/banner_sm.webp';
 import { ContentLayout } from './content_layout';
 
 type PageBannerProps = {
@@ -13,10 +13,10 @@ export function PageBanner({ title, description, trailing }: PageBannerProps) {
       backgroundImage: `url(${bannersmall})`,
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
+      backgroundPosition: 'center calc(100% + 180px)',
       maxHeight: 175,
       height: 175,
-      boxShadow: 'inset 0px 175px 0px rgba(0, 0, 0, 0.6)',
+      boxShadow: 'inset 0px 175px 0px rgba(0, 0, 0, 0.62)',
     }}>
       <div className='flex h-full items-end place-content-between gap-4'>
         <div className='flex flex-col justify-center gap-4 h-full w-full'>
