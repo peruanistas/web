d51a4e9379928efe7d06efb747f3d21f09d61985
Paolo Flores <pflores.fisi22@gmail.com>
2025-05-16 19:55:03 -0500

    move constants to separate file

--- src/features/common/components/footer.tsx
+++ src/features/common/components/footer.tsx
@@ -1,5 +1,31 @@
+import { TABS } from '@common/constants';
+import { Link } from 'wouter';
+
 export function Footer() {
+  const year = new Date().getFullYear();
+
   return (
-    <div></div>
+    <footer className='flex flex-col justify-start p-4 bg-white px-page border-t-2 border-border'>
+      <div className='flex items-center border-b-2 border-border pb-6 gap-16'>
+        <img src='/favicon.svg' alt='logo' width={128} />
+        <div className='flex flex-col gap-2'>
+          <div className='font-bold'>MENÚ</div>
+          {
+            TABS.map((tab) => (
+              <Link
+                key={tab.name}
+                href={tab.href}
+                className='flex h-full mr-4'
+              >
+                {tab.name}
+              </Link>
+            ))
+          }
+        </div>
+      </div>
+      <div className='text-sm mt-4'>
+        © {year} El Peruanista. Todos los derechos reservados.
+      </div>
+    </footer>
   );
 }
---
+++ src/features/common/constants.ts
@@ -0,0 +1,7 @@
+export const TABS = [
+  { name: 'Inicio', regex: /\/$/, href: '/' },
+  { name: 'Proyectos', regex: /^\/proyectos/, href: '/proyectos' },
+  // { name: 'Ayllu', regex: /^\/ayllu/, href: '/ayllu' },
+  { name: 'Grupos', regex: /^\/grupos/, href: '/grupos' },
+  { name: 'Eventos', regex: /^\/eventos/, href: '/eventos' },
+];
