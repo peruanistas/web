e7a40c11cc3b7c430ce2cb7c9743e24a093d8717
Paolo Flores <pflores.fisi22@gmail.com>
2025-05-17 20:38:02 -0500

    hide Grupos tab

--- src/features/common/constants.ts
+++ src/features/common/constants.ts
@@ -4,7 +4,7 @@ export const TABS = [
   { name: 'Inicio', regex: /\/$/, href: '/' },
   { name: 'Proyectos', regex: /^\/proyectos/, href: '/proyectos' },
   // { name: 'Ayllu', regex: /^\/ayllu/, href: '/ayllu' },
-  { name: 'Grupos', regex: /^\/grupos/, href: '/grupos' },
+  // { name: 'Grupos', regex: /^\/grupos/, href: '/grupos' },
   { name: 'Eventos', regex: /^\/eventos/, href: '/eventos' },
 ];
 
