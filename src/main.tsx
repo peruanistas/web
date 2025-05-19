a3544f90754634d508d8ceec30a8daf1bd2f9d2e
Paolo Flores <pflores.fisi22@gmail.com>
2025-05-18 20:16:08 -0500

    add tanstack dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { StrictMode } from 'react';
 import { Route, Switch } from 'wouter';
 import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
 import { HomePage } from '@home/pages/home';
 import { SignUpPage } from '@auth/pages/signup';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -15,14 +16,14 @@ import { CompleteRegisterPage } from '@auth/pages/complete_register';
 import { AuthProvider } from '@auth/providers/auth_provider';
 import { ProjectsPage } from '@projects/pages/projects';
 import { useQueryFavicon } from '@hooks/query_favicon';
+import { ArticlePage } from '@news/pages/ArticlePage';
+import NewCreatePage from '@news/pages/new_create';
 
 import 'react-day-picker/style.css';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
-import { ArticlePage } from '@news/pages/ArticlePage';
-import NewCreatePage from '@news/pages/new_create';
 
-export function PeruanistasRoot() {
+export function PeruanistasRouter() {
   useQueryFavicon();
 
   return (
@@ -53,12 +54,36 @@ export function PeruanistasRoot() {
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <PeruanistasRoot />
-      </AuthProvider>
-    </QueryClientProvider>
-  </StrictMode>
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
 );
+
+function PeruanistasRoot() {
+  const [showDevtools, setShowDevtools] = useState(import.meta.env.DEV); // shown by default in DEV
+
+  useEffect(() => {
+    // @ts-expect-error adding a new global function
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
+  return (
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <PeruanistasRouter />
+        </AuthProvider>
+        {showDevtools && (
+          <React.Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </React.Suspense>
+        )}
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<PeruanistasRoot />);
