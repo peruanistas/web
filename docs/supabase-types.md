f80b520cf5abca4d3aa746004ddfb314ecd05828
Paolo Flores <pflores.fisi22@gmail.com>
2025-05-26 17:32:09 -0500

    update supabase types

--- docs/supabase-types.md
+++ docs/supabase-types.md
@@ -2,7 +2,6 @@
 
 ```sh
 npx supabase login
-npx supabase init
 
 npx supabase gen types typescript --project-id "$PROJECT_ID" --schema public > src/services/db/schema.ts
 ```
--- src/services/db/schema.ts
+++ src/services/db/schema.ts
@@ -36,25 +36,25 @@ export type Database = {
         }
         Relationships: [
           {
-            foreignKeyName: "comments_author_id_fkey"
-            columns: ["author_id"]
+            foreignKeyName: 'comments_author_id_fkey'
+            columns: ['author_id']
             isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
+            referencedRelation: 'profiles'
+            referencedColumns: ['id']
           },
           {
-            foreignKeyName: "comments_event_id_fkey"
-            columns: ["event_id"]
+            foreignKeyName: 'comments_event_id_fkey'
+            columns: ['event_id']
             isOneToOne: false
-            referencedRelation: "events"
-            referencedColumns: ["id"]
+            referencedRelation: 'events'
+            referencedColumns: ['id']
           },
           {
-            foreignKeyName: "comments_project_id_fkey"
-            columns: ["project_id"]
+            foreignKeyName: 'comments_project_id_fkey'
+            columns: ['project_id']
             isOneToOne: false
-            referencedRelation: "projects"
-            referencedColumns: ["id"]
+            referencedRelation: 'projects'
+            referencedColumns: ['id']
           },
         ]
       }
@@ -74,7 +74,7 @@ export type Database = {
           published_at: string
           title: string
           updated_at: string
-          visibility: Database["public"]["Enums"]["visibility"]
+          visibility: Database['public']['Enums']['visibility']
         }
         Insert: {
           active?: boolean
@@ -91,7 +91,7 @@ export type Database = {
           published_at: string
           title: string
           updated_at?: string
-          visibility?: Database["public"]["Enums"]["visibility"]
+          visibility?: Database['public']['Enums']['visibility']
         }
         Update: {
           active?: boolean
@@ -108,22 +108,22 @@ export type Database = {
           published_at?: string
           title?: string
           updated_at?: string
-          visibility?: Database["public"]["Enums"]["visibility"]
+          visibility?: Database['public']['Enums']['visibility']
         }
         Relationships: [
           {
-            foreignKeyName: "events_geo_department_fkey"
-            columns: ["geo_department"]
+            foreignKeyName: 'events_geo_department_fkey'
+            columns: ['geo_department']
             isOneToOne: false
-            referencedRelation: "geo_pe_departments"
-            referencedColumns: ["code"]
+            referencedRelation: 'geo_pe_departments'
+            referencedColumns: ['code']
           },
           {
-            foreignKeyName: "events_geo_district_fkey"
-            columns: ["geo_district"]
+            foreignKeyName: 'events_geo_district_fkey'
+            columns: ['geo_district']
             isOneToOne: false
-            referencedRelation: "geo_pe_districts"
-            referencedColumns: ["code"]
+            referencedRelation: 'geo_pe_districts'
+            referencedColumns: ['code']
           },
         ]
       }
@@ -199,18 +199,18 @@ export type Database = {
         }
         Relationships: [
           {
-            foreignKeyName: "profiles_geo_department_fkey"
-            columns: ["geo_department"]
+            foreignKeyName: 'profiles_geo_department_fkey'
+            columns: ['geo_department']
             isOneToOne: false
-            referencedRelation: "geo_pe_departments"
-            referencedColumns: ["code"]
+            referencedRelation: 'geo_pe_departments'
+            referencedColumns: ['code']
           },
           {
-            foreignKeyName: "profiles_geo_district_fkey"
-            columns: ["geo_district"]
+            foreignKeyName: 'profiles_geo_district_fkey'
+            columns: ['geo_district']
             isOneToOne: false
-            referencedRelation: "geo_pe_districts"
-            referencedColumns: ["code"]
+            referencedRelation: 'geo_pe_districts'
+            referencedColumns: ['code']
           },
         ]
       }
@@ -225,11 +225,11 @@ export type Database = {
           id: string
           image_url: string | null
           impression_count: number
-          ioarr_type: Database["public"]["Enums"]["ioarr_type"]
+          ioarr_type: Database['public']['Enums']['ioarr_type']
           published_at: string
           title: string
           updated_at: string
-          visibility: Database["public"]["Enums"]["visibility"]
+          visibility: Database['public']['Enums']['visibility']
         }
         Insert: {
           active?: boolean
@@ -241,11 +241,11 @@ export type Database = {
           id?: string
           image_url?: string | null
           impression_count?: number
-          ioarr_type: Database["public"]["Enums"]["ioarr_type"]
+          ioarr_type: Database['public']['Enums']['ioarr_type']
           published_at: string
           title: string
           updated_at?: string
-          visibility?: Database["public"]["Enums"]["visibility"]
+          visibility?: Database['public']['Enums']['visibility']
         }
         Update: {
           active?: boolean
@@ -257,26 +257,26 @@ export type Database = {
           id?: string
           image_url?: string | null
           impression_count?: number
-          ioarr_type?: Database["public"]["Enums"]["ioarr_type"]
+          ioarr_type?: Database['public']['Enums']['ioarr_type']
           published_at?: string
           title?: string
           updated_at?: string
-          visibility?: Database["public"]["Enums"]["visibility"]
+          visibility?: Database['public']['Enums']['visibility']
         }
         Relationships: [
           {
-            foreignKeyName: "projects_geo_department_fkey"
-            columns: ["geo_department"]
+            foreignKeyName: 'projects_geo_department_fkey'
+            columns: ['geo_department']
             isOneToOne: false
-            referencedRelation: "geo_pe_departments"
-            referencedColumns: ["code"]
+            referencedRelation: 'geo_pe_departments'
+            referencedColumns: ['code']
           },
           {
-            foreignKeyName: "projects_geo_district_fkey"
-            columns: ["geo_district"]
+            foreignKeyName: 'projects_geo_district_fkey'
+            columns: ['geo_district']
             isOneToOne: false
-            referencedRelation: "geo_pe_districts"
-            referencedColumns: ["code"]
+            referencedRelation: 'geo_pe_districts'
+            referencedColumns: ['code']
           },
         ]
       }
@@ -313,7 +313,7 @@ export type Database = {
       publications: {
         Row: {
           active: boolean
-          author_id: string
+          author_id: string | null
           content: string
           created_at: string
           downvotes: number
@@ -327,11 +327,11 @@ export type Database = {
           title: string
           updated_at: string
           upvotes: number
-          visibility: Database["public"]["Enums"]["visibility"]
+          visibility: Database['public']['Enums']['visibility']
         }
         Insert: {
           active?: boolean
-          author_id: string
+          author_id?: string | null
           content: string
           created_at?: string
           downvotes?: number
@@ -345,11 +345,11 @@ export type Database = {
           title: string
           updated_at?: string
           upvotes?: number
-          visibility?: Database["public"]["Enums"]["visibility"]
+          visibility?: Database['public']['Enums']['visibility']
         }
         Update: {
           active?: boolean
-          author_id?: string
+          author_id?: string | null
           content?: string
           created_at?: string
           downvotes?: number
@@ -363,15 +363,22 @@ export type Database = {
           title?: string
           updated_at?: string
           upvotes?: number
-          visibility?: Database["public"]["Enums"]["visibility"]
+          visibility?: Database['public']['Enums']['visibility']
         }
         Relationships: [
           {
-            foreignKeyName: "publications_source_id_fkey"
-            columns: ["source_id"]
+            foreignKeyName: 'publications_author_id_fkey'
+            columns: ['author_id']
             isOneToOne: false
-            referencedRelation: "publication_sources"
-            referencedColumns: ["id"]
+            referencedRelation: 'profiles'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'publications_source_id_fkey'
+            columns: ['source_id']
+            isOneToOne: false
+            referencedRelation: 'publication_sources'
+            referencedColumns: ['id']
           },
         ]
       }
@@ -384,12 +391,12 @@ export type Database = {
     }
     Enums: {
       ioarr_type:
-        | "investment"
-        | "optimization"
-        | "extension"
-        | "repair"
-        | "replacement"
-      visibility: "draft" | "public" | "private"
+      | 'investment'
+      | 'optimization'
+      | 'extension'
+      | 'repair'
+      | 'replacement'
+      visibility: 'draft' | 'public' | 'private'
     }
     CompositeTypes: {
       [_ in never]: never
@@ -397,122 +404,122 @@ export type Database = {
   }
 }
 
-type DefaultSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, 'public'>]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
-    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof Database },
+  | keyof (DefaultSchema['Tables'] & DefaultSchema['Views'])
+  | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+  ? keyof (Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+    Database[DefaultSchemaTableNameOrOptions['schema']]['Views'])
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+  ? (Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+    Database[DefaultSchemaTableNameOrOptions['schema']]['Views'])[TableName] extends {
       Row: infer R
     }
-    ? R
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])
-    ? (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
-    : never
+  ? R
+  : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema['Tables'] &
+    DefaultSchema['Views'])
+  ? (DefaultSchema['Tables'] &
+    DefaultSchema['Views'])[DefaultSchemaTableNameOrOptions] extends {
+      Row: infer R
+    }
+  ? R
+  : never
+  : never
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema['Tables']
+  | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+  ? keyof Database[DefaultSchemaTableNameOrOptions['schema']]['Tables']
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
-    : never
+  ? Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+    Insert: infer I
+  }
+  ? I
+  : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+  ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
+    Insert: infer I
+  }
+  ? I
+  : never
+  : never
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema['Tables']
+  | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+  ? keyof Database[DefaultSchemaTableNameOrOptions['schema']]['Tables']
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
-    : never
+  ? Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+    Update: infer U
+  }
+  ? U
+  : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+  ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
+    Update: infer U
+  }
+  ? U
+  : never
+  : never
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
-    | keyof DefaultSchema["Enums"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema['Enums']
+  | { schema: keyof Database },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+  ? keyof Database[DefaultSchemaEnumNameOrOptions['schema']]['Enums']
+  : never = never,
 > = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+  ? Database[DefaultSchemaEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema['Enums']
+  ? DefaultSchema['Enums'][DefaultSchemaEnumNameOrOptions]
+  : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema['CompositeTypes']
+  | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
+  ? keyof Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
+  : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+  ? Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema['CompositeTypes']
+  ? DefaultSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
+  : never
 
 export const Constants = {
   public: {
     Enums: {
       ioarr_type: [
-        "investment",
-        "optimization",
-        "extension",
-        "repair",
-        "replacement",
+        'investment',
+        'optimization',
+        'extension',
+        'repair',
+        'replacement',
       ],
-      visibility: ["draft", "public", "private"],
+      visibility: ['draft', 'public', 'private'],
     },
   },
-} as const
+} as const;
