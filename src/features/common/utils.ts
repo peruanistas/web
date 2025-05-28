3eb00ab8d4301465d4ac9e419ca27edbd8429478
Paolo Flores <pflores.fisi22@gmail.com>
2025-05-27 23:01:35 -0500

    fix redirect url

--- src/features/common/utils.ts
+++ src/features/common/utils.ts
@@ -71,9 +71,7 @@ export function mergeAndShuffle<T>(a: T[], b: T[]): T[] {
 }
 
 export function getRedirectURL() {
-  let url = import.meta.env.DEV ? 'http://localhost:3000/' : 'https://peruanistas.pages.dev/';
-  // Make sure to include `https://` when not localhost.
-  url = url.startsWith('http') ? url : `https://${url}`;
+  let url = import.meta.env.DEV ? 'http://localhost:5173/' : 'https://peruanistas.pages.dev/';
   // Make sure to include a trailing `/`.
   url = url.endsWith('/') ? url : `${url}/`;
   return url;
