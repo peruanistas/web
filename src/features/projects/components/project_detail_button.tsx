1647d6ba5a61dfe52bd2a6c2bee440e3bc49ab95
Paolo Flores <pflores.fisi22@gmail.com>
2025-06-13 20:55:18 -0500

    fix layout shift in project detail

--- src/features/projects/components/project_detail_button.tsx
+++ src/features/projects/components/project_detail_button.tsx
@@ -1,60 +1,61 @@
+import { Loader } from '@common/components/loader';
 import { useState } from 'react';
 
 type ProjectDetailButtonProps = {
-    title: string;
-    disabled?: boolean;
-    loading?: boolean;
-    theme?: 'main' | 'secondary';
-    onClick?: () => Promise<void>;
+  title: string;
+  disabled?: boolean;
+  loading?: boolean;
+  theme?: 'main' | 'secondary';
+  onClick?: () => Promise<void>;
 };
 
 export default function ProjectDetailButton({
-    title,
-    disabled = false,
-    loading = false,
-    theme = 'main',
-    onClick=async ()=>{}
+  title,
+  disabled = false,
+  loading = false,
+  theme = 'main',
+  onClick = async () => { }
 }: ProjectDetailButtonProps) {
-    const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-    const buttonClass =
-        theme === 'main'
-            ? 'project_detail_button main'
-            : 'project_detail_button secondary';
+  const buttonClass =
+    theme === 'main'
+      ? 'project_detail_button main'
+      : 'project_detail_button secondary';
 
-    if (disabled) {
-        return (
-            <button className={`${buttonClass} bg-gray-300! text-gray-600! cursor-not-allowed`} disabled>
-                {
-                    isLoading || loading ? (
-                        <span className="loader"></span>
-                    ) : (
-                        <h3>{title}</h3>
-                    )
-                }
-            </button>
-        );
-    }
-
+  if (disabled) {
     return (
-        <button className={buttonClass} onClick={async () => {
-            try {
-                setLoading(true);
-                await onClick();
-            } catch (error) {
-                console.error(error);
-                setLoading(false);
-            } finally {
-                setLoading(false);
-            }
-        }}>
-            {
-                isLoading || loading ? (
-                    <span className="loader"></span>
-                ) : (
-                    <h3>{title}</h3>
-                )
-            }
-        </button>
+      <button className={`${buttonClass} cursor-not-allowed flex items-center justify-center h-14 bg-gray-300! text-gray-600!`} disabled>
+        {
+          isLoading || loading ? (
+            <Loader />
+          ) : (
+            <h3>{title}</h3>
+          )
+        }
+      </button>
     );
+  }
+
+  return (
+    <button className={`${buttonClass} cursor-pointer flex items-center justify-center h-14`} onClick={async () => {
+      try {
+        setLoading(true);
+        await onClick();
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    }}>
+      {
+        isLoading || loading ? (
+          <Loader variant='white' />
+        ) : (
+          <h3>{title}</h3>
+        )
+      }
+    </button>
+  );
 }
