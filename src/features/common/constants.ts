4db3a0b535979df404e95ad57a82525ca777f41b
Paolo Flores <pflores.fisi22@gmail.com>
2025-06-19 23:54:08 -0500

    add ayllu nav

--- src/features/common/constants.ts
+++ src/features/common/constants.ts
@@ -3,9 +3,10 @@ import NO_IMAGE_URL from '@assets/images/no_image.jpg';
 export const TABS = [
   { name: 'Inicio', regex: /\/$/, href: '/' },
   { name: 'Proyectos', regex: /^\/proyectos/, href: '/proyectos' },
-  // { name: 'Ayllu', regex: /^\/ayllu/, href: '/ayllu' },
   { name: 'Grupos', regex: /^\/grupos/, href: '/grupos' },
   { name: 'Eventos', regex: /^\/eventos/, href: '/eventos' },
+  { name: 'Megaproyectos', regex: /^\/megaproyectos/, href: '/megaproyectos' },
+  { name: 'Ayllu', regex: /^\/ayllu/, href: '/ayllu' },
 ];
 
 export { NO_IMAGE_URL };
