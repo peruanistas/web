072bc41b12d4a4fe73c5692f0ed0425a6d5d1da6
Paolo Flores <pflores.fisi22@gmail.com>
2025-08-15 14:31:44 -0500

    reorder tabs

--- src/features/common/constants.ts
+++ src/features/common/constants.ts
@@ -1,12 +1,12 @@
 import NO_IMAGE_URL from '@assets/images/no_image.jpg';
 
 export const TABS = [
-  { name: 'Inicio', regex: /\/$/, href: '/' },
-  { name: 'Proyectos', regex: /^\/proyectos/, href: '/proyectos' },
   { name: 'Grupos', regex: /^\/grupos/, href: '/grupos' },
+  { name: 'Proyectos', regex: /^\/proyectos/, href: '/proyectos' },
   { name: 'Eventos', regex: /^\/eventos/, href: '/eventos' },
-  { name: 'Megaproyectos', regex: /^\/megaproyectos/, href: '/megaproyectos' },
-  { name: 'Ayllu', regex: /^\/ayllu/, href: '/ayllu' },
+  { name: 'Noticias', regex: /\/$/, href: '/' },
+  // { name: 'Megaproyectos', regex: /^\/megaproyectos/, href: '/megaproyectos' },
+  // { name: 'Ayllu', regex: /^\/ayllu/, href: '/ayllu' },
 ];
 
 export { NO_IMAGE_URL };
